f4d96c7a1f7289559fd36b66bee178c182ee82f6
njetami <107357321+njetami@users.noreply.github.com>
2022-06-12 11:28:03 -0400

    Add files via upload

---
+++ YearlyMaintenanceCost.java
@@ -0,0 +1,17 @@
+public class YearlyMaintenanceCost {
+    public static void main(String[] args)
+    {
+        double winterCost = 500;
+        double springCost = 400;
+        double summerCost = 200;
+        double fallCost =350;
+        double totalYearlyMaintenanceCost ;
+
+        totalYearlyMaintenanceCost = winterCost + springCost + summerCost + fallCost;
+        System.out.println( "The total yearly maintenance cost of four seasons is " + totalYearlyMaintenanceCost);
+        System.out.println("The total yearly maintenance cost of four seasons " + winterCost + " " + springCost + " " + summerCost + " " + fallCost + " is " + totalYearlyMaintenanceCost);
+
+
+
+    }
+}
